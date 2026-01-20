bff51ae451f970f279a930351e0b3ab34a852f97
Aura Cho <acho1833@inst.hcpss.org>
2026-01-20 14:48:31 +0000

    feat(models): add workspace response model for external API

---
+++ src/models/workspace-response.model.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+import { entityResponseSchema } from './entity-response.model';
+
+/**
+ * Relationship from external API.
+ */
+export interface RelationshipResponse {
+  relationshipId: string;
+  predicate: string;
+  sourceEntityId: string;
+  relatedEntityId: string;
+}
+
+export const relationshipResponseSchema = z.object({
+  relationshipId: z.string(),
+  predicate: z.string(),
+  sourceEntityId: z.string(),
+  relatedEntityId: z.string()
+});
+
+/**
+ * Workspace response from external API.
+ * Does not include viewPreference - that's stored in our DB.
+ */
+export interface WorkspaceResponse {
+  id: string;
+  name: string;
+  entityList: z.infer<typeof entityResponseSchema>[];
+  relationshipList: RelationshipResponse[];
+}
+
+export const workspaceResponseSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  entityList: entityResponseSchema.array(),
+  relationshipList: relationshipResponseSchema.array()
+});
